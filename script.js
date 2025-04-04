1bca9db0c857723fb78181f1a75777c8ab9ba762
Deepak261220 <deepakcheemakurthi1@gmail.com>
2025-04-04 13:49:57 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('search-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const searchValue = document.getElementById('search').value;
-  const categoryValue = document.getElementById('category').value;
-
-  alert('Search initiated for: ' + searchValue + ' in category: ' + categoryValue);
-});
